b8ecc9275aa0b1acdd445c8ed0797cfaf1327dee
mkhant <minkhantmaw@onenex.co>
2025-01-08 21:05:21 +0630

    setup ui

--- resources/views/livewire/item-card.blade.php
+++ resources/views/livewire/item-card.blade.php
@@ -1,10 +1,21 @@
-<div class="">
+<div class="bg-gray-100 shadow-sm rounded-lg">
     <div>
-        <img src="{{ asset('images/images.jpg') }}" alt="product-image" class="rounded object-cover" />
+        <img src="{{ asset('images/images.jpg') }}" alt="product-image" class="rounded-t-lg object-cover" height="400px" width="400px" />
     </div>
     <div>
-        <h2>
-
+        <h2 class="line-clamp-2 px-3">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy
         </h2>
+        <div class="flex justify-between px-3 py-2">
+            <div class="bg-green-200 p-1 rounded-md">
+                <h2 class="text-1xl">Outfit</h2>
+            </div>
+            <h2 class="font-medium">$ 2.22</h2>
+        </div>
+        <div class="px-3 py-2">
+            <a class="block w-full rounded bg-blue-600 px-12 py-2 text-sm font-medium">
+                Add to Cart
+            </a>
+        </div>
     </div>
 </div>
