5bed7df9a6711c48e353916784babaa7f03e82cd
mkhant <minkhantmaw@onenex.co>
2025-01-12 16:29:59 +0630

    product listing and product details

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,16 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" xmlns:livewire="http://www.w3.org/1999/html">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Ecommerce</title>
-        @vite('resources/css/app.css')
-    </head>
-    <body class="">
-        <livewire:header/>
-        <livewire:hero-section/>
-        <livewire:product-section/>
-    </body>
+    <title>Ecommerce</title>
+    @vite('resources/css/app.css')
+</head>
+<body class="">
+    <livewire:header />
+    <livewire:hero-section />
+    <livewire:product-section />
+    <livewire:footer />
+</body>
 </html>
