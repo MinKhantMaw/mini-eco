5f3da8db526c34184be2a26aea23d5a378b195f3
mkhant <minkhantmaw@onenex.co>
2025-01-13 22:21:44 +0630

    auth setup

--- resources/views/livewire/header.blade.php
+++ resources/views/livewire/header.blade.php
@@ -30,9 +30,24 @@
 
             <div class="flex items-center gap-4">
                 <div class="sm:flex sm:gap-4">
-                    <a class="block rounded-md bg-blue-600 px-5 py-2.5 text-sm font-medium text-white transition hover:bg-blue-700" href="#">
+                    @if (auth()->check())
+                    <a href="/cart">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 mt-2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+                        </svg>
+                    </a>
+
+                    <a href="/auth/logout">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 mt-2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15m3 0 3-3m0 0-3-3m3 3H9" />
+                        </svg>
+
+                    </a>
+                    @else
+                    <a class="block rounded-md bg-blue-600 px-5 py-2.5 text-sm font-medium text-white transition hover:bg-blue-700" href="/auth/login">
                         Get Started
                     </a>
+                    @endif
 
                     {{-- <a--}}
                     {{-- class="hidden rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600 transition hover:text-teal-600/75 sm:block"--}}
--- resources/views/livewire/item-card.blade.php
+++ resources/views/livewire/item-card.blade.php
@@ -1,27 +1,27 @@
 <div class="bg-gray-100 shadow-sm rounded-lg">
-   <a href="/product/details">
-       <div>
-           <img src="{{ asset('images/images.jpg') }}" alt="product-image" class="rounded-t-lg object-cover" height="400px" width="400px" />
-       </div>
-       <div>
-           <h2 class="line-clamp-2 px-3">
-               Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy
-           </h2>
-           <div class="flex justify-between px-3 py-2">
-               <div class="bg-green-200 p-1 rounded-md">
-                   <h2 class="text-1xl">Outfit</h2>
-               </div>
-               <h2 class="font-medium">$ 2.22</h2>
-           </div>
-           <div class="flex gap-2  justify-center
+    <a href="/product/details">
+        <div>
+            <img src="{{ asset('images/images.jpg') }}" alt="product-image" class="rounded-t-lg object-cover" height="400px" width="400px" />
+        </div>
+        <div>
+            <h2 class="line-clamp-2 px-3">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy
+            </h2>
+            <div class="flex justify-between px-3 py-2">
+                <div class="bg-green-200 p-1 rounded-md">
+                    <h2 class="text-1xl">Outfit</h2>
+                </div>
+                <h2 class="font-medium">$ 2.22</h2>
+            </div>
+            <div class="flex gap-2  justify-center
          w-full rounded bg-blue-600 px-12 py-2 text-sm font-medium">
-               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                   <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
-               </svg>
-               <a class="">
-                   Add to Cart
-               </a>
-           </div>
-       </div>
-   </a>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+                </svg>
+                <a class="" href="{{ auth()->check() ? '/cart' : '/auth/login' }}">
+                    Add to Cart
+                </a>
+            </div>
+        </div>
+    </a>
 </div>
